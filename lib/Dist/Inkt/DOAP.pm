ce1e766e0610ad2b72c898e7430411317929064b
Toby Inkster <mail@tobyinkster.co.uk>
2014-05-26 22:31:18 +0100

    initial commit

---
+++ lib/Dist/Inkt/DOAP.pm
@@ -0,0 +1,47 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package Dist::Inkt::DOAP;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = '0.016';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Inkt::DOAP - various DOAP-related roles for Dist::Inkt
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Dist-Inkt-DOAP>.
+
+=head1 SEE ALSO
+
+L<Dist::Inkt>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2014 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
