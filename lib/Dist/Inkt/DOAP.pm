9da337899989d687ece2002083015bf550bd0b2a
Toby Inkster <mail@tobyinkster.co.uk>
2014-08-12 23:39:40 +0100

    add a DESCRIPTION

--- lib/Dist/Inkt/DOAP.pm
+++ lib/Dist/Inkt/DOAP.pm
@@ -19,6 +19,34 @@ __END__
 
 Dist::Inkt::DOAP - various DOAP-related roles for Dist::Inkt
 
+=head1 DESCRIPTION
+
+Dist::Inkt is a framework for writing your own distribution builder.
+It's a simple class that makes an empty directory, creates a MANIFEST
+file listing the contents of the directory, and then compresses it
+into a tarball.
+
+Which seems pretty useless. However, it provides tonnes of hooks that
+can be used by roles applied to the class. This distribution provides a
+collection of roles that help you populate the distribution's metadata
+(META.json, Changes, etc) from DOAP.
+
+=head2 The Straight DOAP
+
+So what is DOAP? This explanation is lifted from
+L<Wikipedia|http://en.wikipedia.org/wiki/DOAP>.
+
+I<< DOAP (Description of a Project) is an RDF Schema and XML vocabulary
+to describe software projects, in particular free and open source
+software. >>
+
+I<< It was created and initially developed by Edd Dumbill to convey
+semantic information associated with open source software projects. >>
+
+I<< It is currently used in the Mozilla Foundation's project page and
+in several other software repositories, notably the Python Package
+Index. >>
+
 =head1 BUGS
 
 Please report any bugs to
