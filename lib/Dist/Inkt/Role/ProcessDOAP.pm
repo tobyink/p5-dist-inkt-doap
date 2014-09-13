c64792ecb02fbce8ea561497f8acaa84404aa7a8
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-13 22:31:38 +0100

    add support for x_IRC

--- lib/Dist/Inkt/Role/ProcessDOAP.pm
+++ lib/Dist/Inkt/Role/ProcessDOAP.pm
@@ -138,21 +138,26 @@ sub cpanmeta_resources
 		}
 	}
 	
-	($resources{X_mailinglist}) =
+	($resources{x_mailinglist}) =
 		map  { $_->uri }
 		grep defined,
 		$self->doap_project->mailing_list;
 	
-	($resources{X_wiki}) =
+	($resources{x_wiki}) =
 		map  { $_->uri }
 		grep defined,
 		$self->doap_project->wiki;
-
-	($resources{X_identifier}) =
+	
+	($resources{x_identifier}) =
 		map  { $_->uri }
 		grep defined,
 		$self->doap_project->rdf_about;
-
+	
+	($resources{x_IRC})
+		map  { $_->uri }
+		grep defined,
+		$self->model->objects(RDF::Trine::iri($self->project_uri), $CPAN->x_IRC);
+	
 	delete $resources{$_} for grep !defined $resources{$_}, keys %resources;
 	
 	return \%resources;
