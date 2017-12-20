865d90ced2d98a604f6bfc0a4eea8627cbd37852
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2017-12-21 00:08:41 +0100

    Add myself as author

--- lib/Dist/Inkt/DOAP.pm
+++ lib/Dist/Inkt/DOAP.pm
@@ -61,10 +61,11 @@ L<Dist::Inkt>.
 =head1 AUTHOR
 
 Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+Kjetil Kjernsmo E<lt>kjetilk@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2014 by Toby Inkster.
+This software is copyright (c) 2014 by Toby Inkster, 2017 by Kjetil Kjernsmo
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
